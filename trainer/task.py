865055c8a62d10d36f8950130b9f7fb6657d62d4
yu81 <usui1118@yu81.jp>
2019-09-17 14:23:24 +0900

    Add procedure in training.

--- trainer/task.py
+++ trainer/task.py
@@ -1,19 +1,44 @@
+from typing import List
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, f1_score, matthews_corrcoef
+
 import joblib
 
 
-def fit(clf, train_data, test_data, test_ratio):
+def fit():
+    data = load_iris()
+    train_data = data["data"]
+    test_data = data["target"]
+
     train_X, test_X, train_y, test_y = train_test_split(
-        train_data, test_data, test_size=test_ratio
+        train_data, test_data, test_size=0.8
     )
-    return clf.fit(train_X, train_y), test_X, test_y
+    clf = RandomForestClassifier(n_estimators=10000)
+    clf = clf.fit(train_X, train_y)
+    joblib.dump(clf, "../models/iris_randomforest.joblib")
+    return clf, test_X, test_y
+
+
+def predict(clf, test_X: List[List[float]], test_y: List[List[float]]):
+    predicted_y = clf.predict(test_X)
+    return predicted_y
+
+
+def evaluate(clf, test_y: List[List[float]], predicted_y: List[int]):
+    matrix = confusion_matrix(test_y, predicted_y)
+    print("confusion matrix: {}".format(matrix.ravel()))
+
+    f1_score_result = f1_score(test_y, predicted_y, average="micro")
+    print("F1 score: {}".format(f1_score_result))
+
+    matthwes_c = matthews_corrcoef(test_y, predicted_y)
+    print("Matthews correlation coefficient (MCC): {}".format(matthwes_c))
 
 
 if __name__ == "__main__":
-    data = load_iris()
-    model, test_X, test_y = fit(
-        RandomForestClassifier(), data["data"], data["target"], 0.8
-    )
-    joblib.dump(model, "../models/iris_randomforest.joblib")
+    clf, test_X, test_y = fit()
+    predicted_y = predict(clf, test_X, test_y)
+    evaluate(clf, test_y, predicted_y)
